6ceffd592b077cc87c8ec39c8e2f76079505f48c
SubinHeo <anmedi3623@naver.com>
2026-04-06 16:28:05 +0900

    fix: 채팅 시 도메인 간 쿠키 미전달 문제 해결 (헤더 인증 방식 도입)

--- src/main/java/com/team01/billage/user/dto/Response/SimpleUserInfoResponseDto.java
+++ src/main/java/com/team01/billage/user/dto/Response/SimpleUserInfoResponseDto.java
@@ -7,14 +7,17 @@ import lombok.Getter;
 public class SimpleUserInfoResponseDto {
     private Long userId;
     private String email;
+    private String accessToken;
 
-    public SimpleUserInfoResponseDto(CustomUserDetails userDetails) {
+    public SimpleUserInfoResponseDto(CustomUserDetails userDetails, String accessToken) {
         if (userDetails == null) {
             this.userId = null;
             this.email = null;
+            this.accessToken = null;
         } else {
             this.userId = userDetails.getId();
             this.email = userDetails.getEmail();
+            this.accessToken = accessToken;
         }
     }
 }
